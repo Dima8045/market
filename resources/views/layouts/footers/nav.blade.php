2a89a453b63224d9e6dd3455180568366372603f
Dmitry Derkach <dmitry.derkach@faceit-team.com>
2020-01-23 15:58:53 +0200

    Instaled Argon dashboard

---
+++ resources/views/layouts/footers/nav.blade.php
@@ -0,0 +1,9 @@
+<div class="row align-items-center justify-content-xl-between">
+    <div class="col-xl-6">
+        <div class="copyright text-center text-xl-left text-muted">
+            &copy; {{ now()->year }} <a href="https://github.com/Dima8045" class="font-weight-bold ml-1" target="_blank">DmitryDerkach</a>
+        </div>
+    </div>
+    <div class="col-xl-6">
+    </div>
+</div>
